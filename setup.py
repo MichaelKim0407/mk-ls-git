b4884f3f985c45e3dfdfe8da671adcecb153f289
Michael Kim <mkim0407@gmail.com>
2017-08-27 15:28:33 -0700

    [INIT] setup pip package

---
+++ setup.py
@@ -0,0 +1,52 @@
+import os
+
+from setuptools import setup, find_packages
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(root_dir, "VERSION")) as f:
+    VERSION = f.read().rstrip()
+
+setup(
+    name="mklsgit",
+
+    version=VERSION,
+
+    install_requires=[
+        'mklibpy>=0.6'
+    ],
+
+    packages=find_packages(),
+
+    entry_points={
+        'console_scripts': [
+            'ls-git=mklsgit:main',
+        ],
+    },
+
+    url="https://github.com/MichaelKim0407/mk-ls-git",
+
+    license="MIT",
+
+    author="Michael Kim",
+
+    author_email="mkim0407@gmail.com",
+
+    description="ls command with git branch",
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+
+        "Topic :: Software Development :: Libraries"
+        "Topic :: Terminals",
+        "Topic :: Utilities",
+    ]
+)
