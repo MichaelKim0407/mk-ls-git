f22d93de70dd1ce12d85e5ae2ca72677b116eda9
Michael Kim <mkim0407@gmail.com>
2017-08-26 17:14:18 -0700

    [FEATURE] basic implementation

---
+++ mk-ls-git.py
@@ -0,0 +1,78 @@
+import os
+import subprocess
+
+from mklibpy.terminal.colored_text import get_text
+from mklibpy.util.path import CD
+
+__author__ = 'Michael'
+
+
+class LsGit(object):
+    def __init__(self, stdout=None):
+        self.__stdout = stdout
+
+    @staticmethod
+    def system_call(*args, **kwargs):
+        out = subprocess.check_output(*args, **kwargs)
+        return out.decode().splitlines(False)
+
+    @staticmethod
+    def is_git_repo(abspath):
+        path = os.path.join(abspath, ".git")
+        return os.path.exists(path) and os.path.isdir(path)
+
+    @staticmethod
+    def get_git_branch(abspath):
+        with CD(abspath):
+            for line in LsGit.system_call(['git', 'branch']):
+                sp = line.split()
+                if "*" not in sp:
+                    continue
+                return sp[1]
+
+    def print(self, *args, **kwargs):
+        print(*args, **kwargs, file=self.__stdout)
+
+    def __call__(self, *args):
+        dirs = [arg for arg in args if not arg.startswith('-')]
+
+        if dirs:
+            cur_dir = dirs[0]
+        else:
+            cur_dir = os.getcwd()
+
+        for line in self.system_call(['ls', '-l'] + list(args)):
+            if line.endswith(':') and line[:-1] in dirs:
+                cur_dir = line[:-1]
+                self.print(line)
+                continue
+
+            sp = line.split()
+            if len(sp) < 9:
+                self.print(line)
+                continue
+
+            dir = sp[8]
+            abspath = os.path.abspath(os.path.join(cur_dir, dir))
+            if not self.is_git_repo(abspath):
+                self.print(line)
+                continue
+
+            branch = self.get_git_branch(abspath)
+            self.print(line + get_text(" ({})".format(branch), color='red'))
+
+
+def main(args=None):
+    if args is None:
+        import sys
+        args = sys.argv[1:]
+
+    instance = LsGit()
+    try:
+        instance(*args)
+    except subprocess.CalledProcessError as e:
+        exit(e.returncode)
+
+
+if __name__ == '__main__':
+    main()
